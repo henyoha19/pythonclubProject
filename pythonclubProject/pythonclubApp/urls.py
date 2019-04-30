00c413efc85fa619049b3aa4edecd6660d9fbee6
Henock W <46727864+henyoha19@users.noreply.github.com>
2019-04-29 21:55:21 -0700

    Add files via upload

--- pythonclubProject/pythonclubApp/urls.py
+++ pythonclubProject/pythonclubApp/urls.py
@@ -4,4 +4,5 @@ from . import views
 #this is a comment
 urlpatterns=[
     path('', views.index, name='index'),
+    path('getResources/', views.getResources, name='resources'),
 ]
--- pythonclubProject/pythonclubApp/views.py
+++ pythonclubProject/pythonclubApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from .models import Meeting,MeetingMinutes,Resource,Event
 # Create your views here.
 def index (request):
     return render(request, 'pythonclubApp/index.html')
+
+def getResources (request):
+    resources_list=Resource.objects.all()
+    return render(request, 'pythonclubApp/resources.html' ,{'resources_list' : resources_list})
+    
