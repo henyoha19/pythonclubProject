1cfe842b6c77233f026ea1da1f7d3a6f7f879be3
Henock W <46727864+henyoha19@users.noreply.github.com>
2019-05-30 11:35:45 -0700

    Add files via upload

--- pythonclubProject/pythonclubApp/tests.py
+++ pythonclubProject/pythonclubApp/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from django.urls import reverse
 from .models import Meeting, MeetingMinutes, Resource, Event
-
+import datetime
+from .forms import ResourceForm
+from django.contrib.auth.models import User
 # Create your tests here.
 class MeetingTest(TestCase):
     def test_string(self):
@@ -37,3 +39,28 @@ class GetMeetingTest(TestCase):
     def test_meeting_detail_sucess(self):
         response=self.client.get(reverse('meetingdetails', args=(self.meet.id,)))
         self.assertEqual(response.status_code, 200)
+# test for forms
+class ResourceFormTest(TestCase):
+    def setUp(self):
+        self.userid2=User.objects.create(username='userid1', password='P@ssw0rd1')
+        
+    
+    def test_resourceForm(self):
+        data={
+            'resourcename' : 'resource1',
+            'resourcetype' : "type1",
+            'userid' : self.userid2,
+            'dateentered' : datetime.date(2019,5,28),
+        }
+        form = ResourceForm(data=data)
+        self.assertTrue(form.is_valid)
+
+    def test_resourceFormInvalid(self):
+        data={
+            'resourcename' : 'resource1',
+            'resourcetype' : 'type1',
+            'userid' : self.userid2,
+            'dateentered' : datetime.date(2019,5,28),
+        }
+        form = ResourceForm(data=data)
+        self.assertFalse(form.is_valid())
